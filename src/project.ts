5b74555c3b503e4e574ac429296fbb0a7611a46c
prime <desmond.akuayi@gmail.com>
2026-01-13 10:01:52 +0000

    Initial commit of Motion Canvas project

--- src/project.ts
+++ src/project.ts
@@ -7,6 +7,7 @@ import vandalism from './scenes/vandalism?scene';
 import perception from './scenes/perception?scene';
 import redemption from './scenes/redemption?scene';
 
+
 export default makeProject({
   // The order here determines the video order
   scenes: [
